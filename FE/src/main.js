61d8b6ad5c1c2ab0bcff062fcd00c98d408ef4b2
unknown <januar.evan2012@gmail.com>
2022-12-09 00:30:47 +0700

    feat: login

--- FE/src/main.js
+++ FE/src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './Apps.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import * as Vue from 'vue'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
-const app = createApp(App)
+const app = Vue.createApp(App)
 
+app.use(VueAxios, axios)
 app.use(router)
 
 app.mount('#app')
